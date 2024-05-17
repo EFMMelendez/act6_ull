e32ec6992b927f71a7862b880ae22220ca3795cd
ELMER_FABIAN_MELENDEZ_MORENO <a.21308051281223@cbtis128.edu.mx>
2024-05-17 17:58:00 +0000

    actualizar

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,198 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      title: 'Refaccionaria',
+      home: MyHomePage(title: 'Refaccionaria'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
   final String title;
-  const MyHomePage({super.key, required this.title});  
+  const MyHomePage({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+      body: PageView(
+        children: [
+          //primera pagina
+          Stack(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                child: Image.asset(
+                  'assets/images/tenis.png',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const Center(
+                child: SizedBox(
+                  width: 150,
+                  height: 200,
+                  child: Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Column(
+                        children: [
+                          Text(
+                            "NIKE",
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          Divider(
+                            thickness: 2,
+                            height: 25,
+                          ),
+                          Icon(Icons.arrow_circle_right),
+                          Text('NIKE\ntenis casuales'),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+          //Segunda pagina
+          Stack(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                child: Image.asset(
+                  'assets/images/tontis.png',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const Center(
+                child: SizedBox(
+                  width: 150,
+                  height: 200,
+                  child: Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Column(
+                        children: [
+                          Text(
+                            'CONVERSE',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          Divider(
+                            thickness: 2,
+                            height: 25,
+                          ),
+                          Icon(Icons.drive_eta),
+                          Text(
+                            'Converse old sckool\ntenis casuales',
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          //Tercera pagina
+          Stack(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                child: Image.asset(
+                  'assets/images/twnis.png',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const Center(
+                child: SizedBox(
+                  width: 150,
+                  height: 200,
+                  child: Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Column(
+                        children: [
+                          Text(
+                            'NIKE',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          Divider(
+                            thickness: 2,
+                            height: 25,
+                          ),
+                          Text(
+                            'NIKE air max\ntenis casuales',
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+          //Cuarta pagina
+          Stack(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                child: Image.asset(
+                  'assets/images/zapato.png',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const Center(
+                child: SizedBox(
+                  width: 150,
+                  height: 200,
+                  child: Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Column(
+                        children: [
+                          Text(
+                            'LUIS VALERIO',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          Divider(
+                            thickness: 2,
+                            height: 25,
+                          ),
+                          Text(
+                            'LUIS VALERIO\zapatos de vestir',
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ],
       ),
     );
   }
